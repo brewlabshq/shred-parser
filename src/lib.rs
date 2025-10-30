fa33f3bcef408f6233eea074e18c2b48a87379d5
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2025-10-30 11:27:22 +0530

    fix: updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use bitflags::bitflags;
-use serde::{Deserialize, Serialize};
 use wincode::{SchemaRead, SchemaWrite};
 
 use crate::signature::Signature;
@@ -7,6 +5,7 @@ pub mod merkle;
 pub mod payload;
 pub mod shred_code;
 pub mod signature;
+
 const SIZE_OF_COMMON_SHRED_HEADER: usize = 83;
 pub const SIZE_OF_DATA_SHRED_HEADERS: usize = 88;
 const SIZE_OF_CODING_SHRED_HEADERS: usize = 89;
@@ -19,38 +18,21 @@ pub const MAX_DATA_SHREDS_PER_SLOT: usize = 32_768;
 pub const MAX_CODE_SHREDS_PER_SLOT: usize = MAX_DATA_SHREDS_PER_SLOT;
 
 #[repr(u8)]
-#[derive(
-    Clone, Copy, Debug, Eq, Hash, PartialEq, Deserialize, Serialize, SchemaWrite, SchemaRead,
-)]
+#[derive(Clone, Copy, Debug, SchemaWrite, SchemaRead)]
 pub enum ShredType {
     Data = 0b1010_0101,
     Code = 0b0101_1010,
 }
 
-#[derive(
-    Clone, Copy, Debug, Eq, Hash, PartialEq, Deserialize, Serialize, SchemaWrite, SchemaRead,
-)]
+#[derive(Clone, Copy, Debug, SchemaWrite, SchemaRead)]
 pub enum ShredVariant {
     MerkleCode { proof_size: u8, resigned: bool }, // 0b01??_????
     MerkleData { proof_size: u8, resigned: bool }, // 0b10??_????
 }
 
-bitflags! {
- #[derive(Clone, Copy, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
-    pub struct ShredFlags:u8 {
-        const SHRED_TICK_REFERENCE_MASK = 0b0011_1111;
-        const DATA_COMPLETE_SHRED       = 0b0100_0000;
-        const LAST_SHRED_IN_SLOT        = 0b1100_0000;
-    }
-
-}
-impl ShredFlags {
-    pub fn from_reference_tick(reference_tick: u8) -> Self {
-        Self::from_bits_retain(Self::SHRED_TICK_REFERENCE_MASK.bits().min(reference_tick))
-    }
-}
+// Shred flags removed for now; use `flags: u8` in headers
 // A common header that is present in data and code shred headers
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, SchemaRead, SchemaWrite)]
 pub struct ShredCommonHeader {
     pub signature: Signature,
     pub shred_variant: ShredVariant,
@@ -61,22 +43,22 @@ pub struct ShredCommonHeader {
 }
 
 /// The data shred header has parent offset and flags
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, SchemaRead, SchemaWrite)]
 pub struct DataShredHeader {
     pub parent_offset: u16,
-    pub flags: u8, // ShredFlags convert
+    pub flags: u8,
     pub size: u16, // common shred header + data shred header + data
 }
 
 /// The coding shred header has FEC information
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, SchemaRead, SchemaWrite)]
 pub struct CodingShredHeader {
     pub num_data_shreds: u16,
     pub num_coding_shreds: u16,
     pub position: u16, // [0..num_coding_shreds)
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
+#[derive(Clone, Debug, SchemaRead, SchemaWrite)]
 pub enum Shred {
     ShredCode(merkle::ShredCode),
     ShredData(merkle::ShredData),
--- src/merkle.rs
+++ src/merkle.rs
@@ -1,15 +1,14 @@
-use serde::{Deserialize, Serialize};
-
 use crate::{payload::Payload, CodingShredHeader, DataShredHeader, ShredCommonHeader};
+use wincode::{SchemaRead, SchemaWrite};
 
-#[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
+#[derive(Clone, Debug, SchemaRead, SchemaWrite)]
 pub struct ShredData {
     pub common_header: ShredCommonHeader,
     pub data_header: DataShredHeader,
     pub payload: Payload,
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
+#[derive(Clone, Debug, SchemaRead, SchemaWrite)]
 pub struct ShredCode {
     pub common_header: ShredCommonHeader,
     pub coding_header: CodingShredHeader,
